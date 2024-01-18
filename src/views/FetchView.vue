faba5cf81d4ce27a5fa6572ed4e53dd4e6d0721f
M-Gharib <mohamad.gharib@ut.ee>
2024-01-18 18:37:39 +0200

    1st

---
+++ src/views/FetchView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="fetch">
+      <h1>This is FetchAPI page</h1>
+      <get-data :posts="posts"/>
+    </div>
+  
+  </template>
+  
+  
+  <script>
+  // @ is an alias to /src
+  import getData from "@/components/getData.vue";   
+  export default {
+    name: "FetchView",
+    components: {
+      getData
+    },
+    data: function() {
+      return {
+      
+      posts:[    
+      ]
+      }
+      },
+      mounted() {
+          fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => response.json())
+          .then(data => this.posts = data)
+          .catch(err => console.log(err.message))
+      }
+  };
+  </script>
+  
